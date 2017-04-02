813b23f0c6737870e39f6c1f67b2dc466aba991a
sabativi <victor.sabatier@gmail.com>
2017-04-02 14:27:01 +0200

    add simple navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Services = () => (
+  <div>
+    <h2>Services</h2>
+  </div>
+)
+
+const Portofolio = ({ match }) => (
+  <div>
+    <h2>Portofolio</h2>
+    <ul>
+      <li>
+        Lovebox
+      </li>
+      <li>
+        Steadxp
+      </li>
+      <li>
+        Webmecanik
+      </li>
+      <li>
+        Grand prix de Serre Chevalier
+      </li>
+    </ul>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/portofolio">Portofolio</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/portofolio" component={Portofolio}/>
+      <Route path="/services" component={Services}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>
+)
 
 export default App;
