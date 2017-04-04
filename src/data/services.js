4bd3b31bc31c12e3b2977fc6447fa20e7e6f32d6
sabativi <victor.sabatier@gmail.com>
2017-04-04 09:21:03 +0200

    add example in services

--- src/data/services.js
+++ src/data/services.js
@@ -4,22 +4,26 @@ const services = [
     description: `I can build you a prototype or a minimal viable product (MVP)for your idea.
                   An MVP can validate your idea and help you raise funding.
                   The code I deliver will be the best possible foundations from which your application and idea can grow.`,
+    example: ['Lovebox', 'ClickToPic'],
   },
   {
     title: 'Enterprise Web Apps',
     description: `Whether you are a Start-up or Bluechip company, I can help you build you a production ready web application.
     The web application will be built with the best technologies and will be scalable, secure and maintainable.`,
+    example: ['Steadxp', 'Campus skills'],
   },
   {
     title: 'Mobile application',
     description: `I can help you build you a production ready mobile application both in Android and iOS.
                   I use React Native to offer the best user experience while having the same codebase between the two.
                   Believe me, it is truly amazing`,
+    example: ['Lovebox', 'Grand Prix Serre Chevalier'],
   },
   {
     title: 'Hybridised Development',
     description: `Do you need a mobile app and web app with the same code base?
                   I can code your web application so that it utilises Cordova to build both a web application and a cross-device mobile application.`,
+    example: ['Campus skills'],
   }
 ];
 
