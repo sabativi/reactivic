89fbea0e290a531cc300cf4e373e24069692d533
sabativi <victor.sabatier@gmail.com>
2017-04-04 09:21:23 +0200

    add logo on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,30 +2,27 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Company from './components/Company';
-import Technology from './components/Technology';
-import technologies from '../data/technologies';
 import companies from '../data/companies';
-
+import Header from '../layouts/Header';
 import './Home.css';
 
 const Hero = () => (
-  <div>
+  <div className='text-center'>
     <h1> Full stack web and mobile developper</h1>
-    <p> I build scalable, maintainable and secure enterprise web and mobile applications for agencies, bluechips, start-ups and sometimes, myself.</p>
+    <h3> I build scalable, maintainable and secure enterprise web and mobile applications for agencies, bluechips, start-ups and sometimes, myself.</h3>
     <LinkContainer to='services'>
-      <p> Check out my <a>services</a> </p>
+      <h5> Check out my <a>services</a> </h5>
     </LinkContainer>
     <LinkContainer to='portofolio'>
-      <p>or my <a>portofolio</a> </p>
+      <h5>or my <a>portofolio</a> </h5>
     </LinkContainer>
   </div>
 )
 
-
 const Companies = ({ companies }) => (
-  <div>
+  <div className='text-center'>
     <h2>
-      Companies i have worked with with :
+      Companies I have worked with :
     </h2>
     {companies.map((company, i) => <Company key={i} company={company}/>)}
   </div>
@@ -34,6 +31,7 @@ const Companies = ({ companies }) => (
 const Home = () => (
   <Row className='home'>
     <Col xs={12} sm={6} className='introLeft'>
+      <Header />
       <Hero />
       <Companies companies={companies} />
     </Col>
