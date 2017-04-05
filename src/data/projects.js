91d2498d7b8614b44764642fed81144f52f1a466
sabativi <victor.sabatier@gmail.com>
2017-04-05 12:29:29 +0200

    change project page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,82 @@
+import WebmecanikImg from './img/projects/webmecanik.png';
+import FrenchTechImg from './img/projects/campus.png';
+import SteadxpImg from './img/projects/steadxp.png';
+import LoveboxImg from './img/projects/lovebox.jpg';
+import SerreCheImg from './img/projects/serreche.png';
+import ClickToPicImg from './img/projects/clicktopic.png';
+
 const projects = [
   {
     title: 'Lovebox mobile application.',
+    description: `blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq`,
+    technos: ['Ionic', 'MeteorJs'],
+    link: 'http://lovebox.love',
+    quote: {
+      content: 'I am so happy',
+      who: 'Jean Gregoire',
+      position: 'CEO',
+    },
+    image: LoveboxImg,
+  },
+  {
+    title: 'Campus skills.',
+    description: `blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq`,
+    technos: ['MeteorJs', 'React'],
+    link: 'http://campus-skills.meteorapp.com',
+    quote: {
+      content: 'His ability to work quickly really made a big difference for our project',
+      who: 'François Albrieux',
+      position: 'Director',
+    },
+    image: FrenchTechImg,
+  },
+  {
+    title: 'Grand prix de Serre Chevalier.',
+    description: `Mobile application both iOS and Android plus registration systems.`,
+    technos: ['MeteorJs', 'React', 'React Native'],
+    link: 'https://grandprix-serrechevalier.com/',
+    quote: {
+      content: 'Victor is simply the best',
+      who: 'Laurent Beauvisage',
+      position: 'CEO',
+    },
+    image: SerreCheImg,
+  },
+  {
+    title: 'Webmecanik training and coaching',
+    description: `Help webmecanik to be better blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq blablq`,
+    technos: ['PHP', 'Symfony', 'CI', 'RabbitMQ'],
+    link: 'https://www.webmecanik.com/en/',
+    quote: {
+      content: 'Victor is the best',
+      who: 'Norman Pracht',
+      position: 'Product Owner',
+    },
+    image: WebmecanikImg,
+  },
+  {
+    title: 'ClickToPic.',
+    description: `Web application for selling photos online plus a backend for tagging those photos.`,
+    technos: ['MeteorJs', 'AWS', 'Lambda', 'S3'],
+    link: 'https://clicktopic.mod.bz/',
+    quote: {
+      content: 'Victor is the best',
+      who: 'Ludovic Griboval',
+      position: 'Director',
+    },
+    image: ClickToPicImg,
+  },
+  {
+    title: 'SteadXP license server.',
     description: ``,
-    technos: [],
-    link: '',
-    quote: '',
-    imagePath: '',
+    technos: ['MeteorJs', 'React', 'Docker', 'Bootstrap'],
+    link: 'http://steadxp.com/',
+    quote: {
+      content: 'Really polyvalent',
+      who: 'Adrien Farrugia',
+      position: 'CEO',
+    },
+    image: SteadxpImg,
   },
 ];
 
--- src/pages/components/Project.js
+++ src/pages/components/Project.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import Technology from './Technology';
+import { Row, Col, Image, Button } from 'react-bootstrap';
 
-const Technologies = ({ technologies }) => (
+import './Project.css';
+import Technology from './Technology';
+import Quote from './Quote';
+
+const Technologies = ({ text, technologies }) => (
   <div>
-    {technologies.map((technology, i) => <Technology key={i} technology={technology} />)}
+    {text} : {technologies.map((technology, i) => <Technology key={i} technology={technology} />)}
   </div>
 )
 
-const Project = ({ project }) => (
-  <li>
-    {project.title}
-  </li>
+Technologies.propTypes = {
+  text: React.PropTypes.string,
+  technologies: React.PropTypes.array,
+}
+
+const Project = ({ project, inverse, backgroundColor }) => (
+  <div style={{backgroundColor: backgroundColor}}>
+    <Row className='project'>
+      <Col xs={12} sm={6} smPush={inverse ? 6 : 0}>
+        <Image src={project.image} className='project-image'/>
+      </Col>
+      <Col xs={12} sm={6} smPull={inverse ? 6 : 0}>
+        <h3 className='text-center'>
+          <a target="_blank" href={project.link}>
+            {project.title}
+          </a>
+        </h3>
+        <p>{project.description}</p>
+        <Quote quote={project.quote} />
+        <Technologies text='Project done with' technologies={project.technos} />
+      </Col>
+    </Row>
+  </div>
 )
+
+Project.propTypes = {
+  project: React.PropTypes.object,
+  inverse: React.PropTypes.bool,
+  backgroundColor: React.PropTypes.string,
+}
 
 export default Project;
