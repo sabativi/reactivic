06331f6de2e6a7de51f014c2553d6d981865573b
sabativi <victor.sabatier@gmail.com>
2017-04-05 17:39:30 +0200

    add playbook font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap-theme.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <App />,
