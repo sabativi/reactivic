cc8f65c4174a9a7a7d414f6011b1a9816529ad85
sabativi <victor.sabatier@gmail.com>
2017-04-05 12:30:31 +0200

    change home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Clearfix } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import Company from './components/Company';
-import companies from '../data/companies';
 import Header from '../layouts/Header';
+import services from '../data/services';
+import Service from './components/Service';
 import './Home.css';
 
 const Hero = () => (
   <div className='text-center'>
-    <h3> I build scalable, maintainable and secure enterprise web and mobile applications for agencies, bluechips, start-ups and sometimes, myself.</h3>
-    <LinkContainer to='services'>
-      <h5> Check out my <a>services</a> </h5>
-    </LinkContainer>
+    <h2> I build scalable, maintainable and secure enterprise web and mobile applications for agencies, bluechips, start-ups and sometimes, myself.</h2>
     <LinkContainer to='portofolio'>
-      <h5>or my <a>portofolio</a> </h5>
+      <h4>Check out my <a>portofolio</a> </h4>
     </LinkContainer>
   </div>
 )
 
-const Companies = ({ companies }) => (
-  <div className='text-center'>
-    <h2>
-      Companies I have worked with :
-    </h2>
-    {companies.map((company, i) => <Company key={i} company={company}/>)}
-  </div>
+const Services = ({ services }) => (
+  <Row className='text-center'>
+    <h3>
+      What I am good at :
+    </h3>
+    {services.map((service, i) =>
+      <div>
+        {i % 2===0 ? <Clearfix/> : null}<Service key={i} service={service}/>
+      </div>
+    )}
+  </Row>
 )
 
 const Home = () => (
   <Row className='home'>
     <Col xs={12} sm={6} className='introLeft'>
-      <Header />
+      <Header rightSide={
+          <Col xs={12} sm={8} >
+            <h1 className='text-center'> Full stack web and mobile developper</h1>
+          </Col>
+      }/>
       <Hero />
-      <Companies companies={companies} />
+      <Services services={services} />
     </Col>
     <Col sm={6} xsHidden className='introRight'/>
   </Row>
