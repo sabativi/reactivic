e453f801500bb3a4a8fdc1a18743c92f8973fbc1
sabativi <victor.sabatier@gmail.com>
2017-04-05 12:28:45 +0200

    add carousel bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
 
 ReactDOM.render(
