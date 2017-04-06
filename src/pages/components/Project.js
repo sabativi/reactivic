bcc0dbbb1abe74c74e573701fca4a05855aecb2a
sabativi <victor.sabatier@gmail.com>
2017-04-06 09:54:54 +0200

    Remove text prop on Service component

--- src/pages/components/Project.js
+++ src/pages/components/Project.js
@@ -5,14 +5,13 @@ import './Project.css';
 import Technology from './Technology';
 import Quote from './Quote';
 
-const Technologies = ({ text, technologies }) => (
+const Technologies = ({ technologies }) => (
   <div>
-    {text} : {technologies.map((technology, i) => <Technology key={i} technology={technology} />)}
+    {technologies.map((technology, i) => <Technology key={i} technology={technology} />)}
   </div>
 )
 
 Technologies.propTypes = {
-  text: React.PropTypes.string,
   technologies: React.PropTypes.array,
 }
 
@@ -23,14 +22,14 @@ const Project = ({ project, inverse, backgroundColor }) => (
         <Image src={project.image} className='project-image'/>
       </Col>
       <Col xs={12} sm={6} smPull={inverse ? 6 : 0}>
-        <h3 className='text-center'>
-          <a target="_blank" href={project.link}>
+        <h3 className='text-center ' >
+          <a className='blue' target="_blank" href={project.link}>
             {project.title}
           </a>
         </h3>
         <p>{project.description}</p>
         <Quote quote={project.quote} />
-        <Technologies text='Project done with' technologies={project.technos} />
+        <Technologies technologies={project.technos} />
       </Col>
     </Row>
   </div>
