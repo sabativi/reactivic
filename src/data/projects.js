ef05913c7e723d341f22473c484e703cedd4ff89
sabativi <victor.sabatier@gmail.com>
2019-02-22 15:56:44 +0100

    update porject and talk

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,135 +1,154 @@
-import WebmecanikImg from './img/projects/webmecanik.png';
-import FrenchTechImg from './img/projects/campus.png';
-import SteadxpImg from './img/projects/steadxp.jpg';
-import LoveboxImg from './img/projects/lovebox.png';
-import SerreCheImg from './img/projects/serreche.png';
-import ClickToPicImg from './img/projects/clicktopic.png';
-import Welbees from './img/projects/welbees.png';
-import DigitalTestCenter from './img/projects/digitaltestcenter.png';
-import GitImg from './img/projects/git.jpg';
+import WebmecanikImg from "./img/projects/webmecanik.png";
+import FrenchTechImg from "./img/projects/campus.png";
+import SteadxpImg from "./img/projects/steadxp.jpg";
+import LoveboxImg from "./img/projects/lovebox.png";
+import SerreCheImg from "./img/projects/serreche.png";
+import ClickToPicImg from "./img/projects/clicktopic.png";
+import Welbees from "./img/projects/welbees.png";
+import Acquereur from "./img/projects/acquereur.png";
+import DigitalTestCenter from "./img/projects/digitaltestcenter.png";
+import GitImg from "./img/projects/git.jpg";
 
 const projects = [
   {
-    title: 'Welbees',
+    title: "L'acquéreur",
+    description: `
+      This project was developped in run mode. A 3 to 4 days commando mission development iteration of your product with an expert team, who will live and work all day long with your own team, in an amazing, unique place.
+      Each run is a delivery iteration of functionnal product, to get feedback ASAP.
+      I love working in such mode.
+    `,
+    technos: ["React", "Meteor", "Algolia", "Run"],
+    link: "https://app.lacquereur.com",
+    quote: {
+      content: ".",
+      who: "Sandrine Michault",
+      position: "Founder"
+    },
+    image: Acquereur
+  },
+  {
+    title: "Welbees",
     description: `
       Iphone, Ipad and Android app, with offline capabilities. Multi langages support. I build the back office using Meteor and setup a graphQL server for the mobile app.
     `,
-    technos: ['React Native', 'Meteor', 'GraphQL'],
-    link: 'https://welbees.com',
+    technos: ["React Native", "Meteor", "GraphQL"],
+    link: "https://welbees.com",
     quote: {
-      content: 'This mobile app was a highly strategic move for our company. Victor has led this important project with great professionalism. Our collaboration started with a rough idea of the main functionalities and Victor provided very valuable insight to this project. I appreciated his ability to adapt to our needs and constraints throughout the course of the project.',
-      who: 'Vira Somvang',
-      position: 'Founder',
+      content:
+        "This mobile app was a highly strategic move for our company. Victor has led this important project with great professionalism. Our collaboration started with a rough idea of the main functionalities and Victor provided very valuable insight to this project. I appreciated his ability to adapt to our needs and constraints throughout the course of the project.",
+      who: "Vira Somvang",
+      position: "Founder"
     },
-    image: Welbees,
+    image: Welbees
   },
   {
-    title: 'Campus skills',
+    title: "Campus skills",
     description: `My own project, to help Schools follow their students' progress.`,
-    technos: ['MeteorJs', 'React'],
-    link: 'http://campus-skills.com',
+    technos: ["MeteorJs", "React"],
+    link: "http://campus-skills.com",
     quote: {
-      content: 'I enjoy working on this projet, and I have happy customers.',
-      who: 'Victor Sabatier :)',
-      position: '',
+      content: "I enjoy working on this projet, and I have happy customers.",
+      who: "Victor Sabatier :)",
+      position: ""
     },
-    image: FrenchTechImg,
+    image: FrenchTechImg
   },
   {
-    title: 'Digital Test Center',
+    title: "Digital Test Center",
     description: `
       Second version of the Digital Test Center app, with a new design, focusing on delivering the best experience possible.
     `,
-    technos: ['React Native', 'GraphQL'],
-    link: 'https://digitaltestcenter.com',
+    technos: ["React Native", "GraphQL"],
+    link: "https://digitaltestcenter.com",
     quote: {
-      content: '',
-      who: 'Laurent Beauvisage',
-      position: 'CEO',
+      content: "",
+      who: "Laurent Beauvisage",
+      position: "CEO"
     },
-    image: DigitalTestCenter,
+    image: DigitalTestCenter
   },
   {
-    title: 'Lovebox mobile application',
+    title: "Lovebox mobile application",
     description: `Lovebox needed both an iOS and an android application to allow the configuration and use of their connected object.
     An MVP had to be done which is now used by more than a thousand people. I have also helped Lovebox to manage the following evolutions internally.
     `,
-    technos: ['Ionic', 'MeteorJs', 'GraphQL'],
-    link: 'http://lovebox.love',
+    technos: ["Ionic", "MeteorJs", "GraphQL"],
+    link: "http://lovebox.love",
     quote: {
-      content: 'Victor worked on our mobile app, his knowledge and his ability to choose the right technologies were very important for us. Plus he is a passionate and enthusiastic person',
-      who: 'Jean Gregoire',
-      position: 'CEO',
+      content:
+        "Victor worked on our mobile app, his knowledge and his ability to choose the right technologies were very important for us. Plus he is a passionate and enthusiastic person",
+      who: "Jean Gregoire",
+      position: "CEO"
     },
-    image: LoveboxImg,
+    image: LoveboxImg
   },
   {
-    title: 'Grand prix de Serre Chevalier',
+    title: "Grand prix de Serre Chevalier",
     description: `Serre Chevalier ski resort wanted to develop an application for participants in their new 'Grand prix de Serre Chevalier'.
     It allows to follow their ranking in real time. It was done in a matter of days, providing a stunning end user experience.`,
-    technos: ['MeteorJs', 'React', 'React Native', 'GraphQL'],
-    link: 'https://grandprix-serrechevalier.com/',
+    technos: ["MeteorJs", "React", "React Native", "GraphQL"],
+    link: "https://grandprix-serrechevalier.com/",
     quote: {
       content: `As a developper, working with Victor was a pleasure. He truly masters what he does. It is someone that you can count on.
                 I wait for the opportunity to work with him again.`,
-      who: 'Laurent Beauvisage',
-      position: 'CEO',
+      who: "Laurent Beauvisage",
+      position: "CEO"
     },
-    image: SerreCheImg,
+    image: SerreCheImg
   },
   {
-    title: 'Webmecanik training and coaching',
+    title: "Webmecanik training and coaching",
     description: `Webmecanik wanted to improve their processes as a software publisher. I joined their team for a few times
                   helping them to tackle technical difficulties.`,
-    technos: ['PHP', 'Symfony', 'CI', 'RabbitMQ'],
-    link: 'https://www.webmecanik.com/en/',
+    technos: ["PHP", "Symfony", "CI", "RabbitMQ"],
+    link: "https://www.webmecanik.com/en/",
     quote: {
       content: `Victor's advices helped us professionalize as a software publisher by adapting our server infrastructures.
                 His multi-technological mastery has also brought us a lot in terms of development`,
-      who: 'Norman Pracht',
-      position: 'CMO & Product Owner',
+      who: "Norman Pracht",
+      position: "CMO & Product Owner"
     },
-    image: WebmecanikImg,
+    image: WebmecanikImg
   },
   {
-    title: 'ClickToPic',
+    title: "ClickToPic",
     description: `Web application for selling photos online plus a backend for tagging those photos.`,
-    technos: ['MeteorJs', 'AWS', 'Lambda'],
-    link: 'https://www.clicktopic.com/',
+    technos: ["MeteorJs", "AWS", "Lambda"],
+    link: "https://www.clicktopic.com/",
     quote: {
       content: `Victor allows us to have a working platform in a month that fits our constaints : be able to handle peak load over a specified period of time.
                Plus Victor is a passionate and always available person. I would definitely recommend.`,
-      who: 'Ludovic Griboval',
-      position: 'Director',
+      who: "Ludovic Griboval",
+      position: "Director"
     },
-    image: ClickToPicImg,
+    image: ClickToPicImg
   },
   {
-    title: 'Git trainer',
+    title: "Git trainer",
     description: `I give git training for more than three years with great successes.`,
-    technos: ['Git'],
-    link: 'http://www.elephorm.com/apprendre-git-fondamentaux',
+    technos: ["Git"],
+    link: "http://www.elephorm.com/apprendre-git-fondamentaux",
     quote: {
       content: `Simple and complete training on the principles of Git. Practical work is a good point that allows to quickly implement the commands previously seen.
                 A trainer who has mastered his subject, and who is a pedagogue. I recommend.`,
-      who: 'A student',
-      position: '',
+      who: "A student",
+      position: ""
     },
-    image: GitImg,
+    image: GitImg
   },
   {
-    title: 'SteadXP',
+    title: "SteadXP",
     description: `Software for videos stabilization and web server for managing licenses.`,
-    technos: ['MeteorJs', 'React', 'Docker', 'C++', 'Qt'],
-    link: 'http://steadxp.com/',
+    technos: ["MeteorJs", "React", "Docker", "C++", "Qt"],
+    link: "http://steadxp.com/",
     quote: {
       content: `Victor’s talent and experience proved immensely valuable for our project.
                 He was involved from the very beginning, providing advice and know a high number of technologies that allow him to solve many problems we faced`,
-      who: 'Adrien Farrugia',
-      position: 'CEO',
+      who: "Adrien Farrugia",
+      position: "CEO"
     },
-    image: SteadxpImg,
-  },
+    image: SteadxpImg
+  }
 ];
 
 export default projects;
--- src/data/talks.js
+++ src/data/talks.js
@@ -1,61 +1,75 @@
 const talks = [
   {
-    title: 'What\'s new in React ?',
-    description: `Some useful tips when using react 16 and more, this is opiniated. I tried to summarize points that I find particularly useful in React 16 and that can be adopt easily in any React application `,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/APAhc5lJSzryjG" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/sabativi/whats-new-in-react-122974180" title="What&#x27;s new in React" target="_blank">What&#x27;s new in React</a> </strong> de <strong><a href="https://www.slideshare.net/sabativi" target="_blank">sabativi</a></strong> </div>',
-    date: '11/2018',
+    title: "L'open source m'a tuer*",
+    description: `Open sourcing something can be hard, in this talk, I give an overview of what happened in my use case`,
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/5fg6VflB7ahATr" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/sabativi/lopen-source-ma-tuer" title="L&#x27;open source m&#x27;a tuer*" target="_blank">L&#x27;open source m&#x27;a tuer*</a> </strong> de <strong><a href="https://www.slideshare.net/sabativi" target="_blank">sabativi</a></strong> </div>',
+    date: "02/2019"
   },
   {
-    title: 'Storybook',
+    title: "What's new in React ?",
+    description: `Some useful tips when using react 16 and more, this is opiniated. I tried to summarize points that I find particularly useful in React 16 and that can be adopt easily in any React application `,
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/APAhc5lJSzryjG" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/sabativi/whats-new-in-react-122974180" title="What&#x27;s new in React" target="_blank">What&#x27;s new in React</a> </strong> de <strong><a href="https://www.slideshare.net/sabativi" target="_blank">sabativi</a></strong> </div>',
+    date: "11/2018"
+  },
+  {
+    title: "Storybook",
     description: `
 In this talk, I try to explain how I use storybook, both in React and React Native environments, benefits I see in writing and maintaining a storybook.
 `,
-    slides: '<h3>This was a full demo presentation so no slides.</h3>',
-    date: '06/2018',
+    slides: "<h3>This was a full demo presentation so no slides.</h3>",
+    date: "06/2018"
   },
   {
-    title: 'React Fiber',
+    title: "React Fiber",
     description: `React Fiber is the new core algorithm behind React reconciliation.
     I wanted to explain what React was and took the challenge that by explaining React Fiber and the reasons why Facebook created it, people could have
     a good understading of the core principles of React librairie.`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/pc97KFoQjuCNq" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
-    date: '05/2017',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/pc97KFoQjuCNq" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
+    date: "05/2017"
   },
   {
-    title: 'React Native.',
-    date: '04/2017',
+    title: "React Native.",
+    date: "04/2017",
     description: `I discovered React Native few months ago and I wanted to explain how it changed the way I build mobile apps today and allow me to deliver a truly native experience at a very competitive cost.`,
-    slides: `<iframe src="//www.slideshare.net/slideshow/embed_code/key/rwccExn729WtED" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>`,
+    slides: `<iframe src="//www.slideshare.net/slideshow/embed_code/key/rwccExn729WtED" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>`
   },
   {
-    title: 'From git to blockchain',
+    title: "From git to blockchain",
     description: `This is one of my favourite talk I have given yet. A truly unique as far as I know where I try to make links between Git and the blockchain. It was well received by the audience. Contact me if you want to know more ;)`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/hVxxngJfoXdVvM" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
-    date: '02/2017',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/hVxxngJfoXdVvM" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
+    date: "02/2017"
   },
   {
-    title: 'GraphQL : the new data layer',
+    title: "GraphQL : the new data layer",
     description: `As a Meteor user, I follow with great interests their transition towards a new data layer and possibilities that might arise from here.`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/elfPHMR2mDEsP9" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
-    date: '10/2016',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/elfPHMR2mDEsP9" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>',
+    date: "10/2016"
   },
   {
-    title: 'MeteorJS : the end of javascript fatigue ?',
+    title: "MeteorJS : the end of javascript fatigue ?",
     description: `2016 was the year of Javascript fatigue : new tools every weeks that made it hard to catch up all the times. In this talk, I explain why Meteor is a great way to avoid this.`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/3VmZOgHUvVJ51H" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
-    date: '04/2016',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/3VmZOgHUvVJ51H" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
+    date: "04/2016"
   },
   {
-    title: 'How the f**k is Bitcoin working ?',
+    title: "How the f**k is Bitcoin working ?",
     description: `I fall in love with Bitcoin principles and the technology behind it : this talk is an attempt to explain in 10 minutes how it works.`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/bG5RIP2nY2Nngw" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
-    date: '11/2015',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/bG5RIP2nY2Nngw" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
+    date: "11/2015"
   },
   {
-    title: 'Deeper look into git',
+    title: "Deeper look into git",
     description: `Git is one of my favourite piece of technology, people coming from others VCS sometimes have difficulties understanding what Git really is. I try to explain it from a different perspective.`,
-    slides: '<iframe src="//www.slideshare.net/slideshow/embed_code/key/i8GOU7T4yxm3MI" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
-    date: '02/2013',
+    slides:
+      '<iframe src="//www.slideshare.net/slideshow/embed_code/key/i8GOU7T4yxm3MI" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> ',
+    date: "02/2013"
   }
 ];
 
